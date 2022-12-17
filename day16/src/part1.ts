8601fbdaa38d938badddf33a8e79ef700495b9ae
John Buxton <lqueryvg@gmail.com>
2022-12-17 11:43:26 +0000

    day 16 parse

--- day16/src/part1.ts
+++ day16/src/part1.ts
@@ -1,12 +1,21 @@
 import { readFileSync } from 'fs'
 
 const main = (filename: string) => {
-  const file = readFileSync(filename, 'utf-8')
+  const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
 
-  for (const line of lines) {
-    if (line === '') continue // skip blank lines
-  }
+  const regexp =
+    /Valve (?<name>-?.*) has flow rate=(?<rate>-?.*); tunnels* leads* to valves* (?<exits>-?.*)/
+  const nodes = lines.map((line) => {
+    const match = line.match(regexp)!.groups
+    return {
+      name: match!.name,
+      rate: Number(match!.rate),
+      isOpen: false,
+      exits: match!.exits.split(', '),
+    }
+  })
+  console.log(JSON.stringify(nodes, null, 4))
   return 'TBD'
 }
 
