0920481238fecc5f6a24ad5455a4e4b1dc205200
John Buxton <lqueryvg@gmail.com>
2022-12-18 20:32:29 +0000

    day 18 part2 tidying

--- day18/src/part2.ts
+++ day18/src/part2.ts
@@ -6,20 +6,18 @@ const getInput = (filename: string) => {
   return lines
 }
 
-const cubes = new Map<string, Cube>()
-class Cube {
+const rockCubes = new Map<string, RockCube>()
+const airCubes = new Set<string>()
+class RockCube {
   x: number
   y: number
   z: number
-  // neighbors: Cube[]
   neighborCount = 0
 
   updateNeighborCountIfExists(x: number, y: number, z: number) {
     const s = `${x},${y},${z}`
-    // console.log({ x, y, z })
-    if (cubes.has(s)) {
-      // console.log('yes')
-      cubes.get(s)!.neighborCount++
+    if (rockCubes.has(s)) {
+      rockCubes.get(s)!.neighborCount++
       return 1
     }
     return 0
@@ -38,20 +36,90 @@ class Cube {
   }
 }
 
+const printBounds = () => {
+  let maxX = Number.MIN_VALUE
+  let minX = Number.MAX_VALUE
+  let maxY = Number.MIN_VALUE
+  let minY = Number.MAX_VALUE
+  let maxZ = Number.MIN_VALUE
+  let minZ = Number.MAX_VALUE
+  for (let cube of rockCubes.values()) {
+    maxX = Math.max(maxX, cube.x)
+    maxY = Math.max(maxY, cube.y)
+    maxZ = Math.max(maxZ, cube.z)
+    minX = Math.min(minX, cube.x)
+    minY = Math.min(minY, cube.y)
+    minZ = Math.min(minZ, cube.z)
+  }
+  console.log({ minX, maxX, minY, maxY, minZ, maxZ })
+}
+
+let totalSurfaceArea = 0
+let min = 0
+let max = 7
+
+const fillAir = (startCoordsString: string) => {
+  const airToExplore = [startCoordsString]
+
+  while (true) {
+    const coordsString = airToExplore.pop()
+    if (!coordsString) break // we are done
+
+    const [x, y, z] = coordsString.split(',').map(Number)
+
+    if (x < min || x > max || y < min || y > max || z < min || z > max) {
+      // console.log('out of bounds')
+      continue
+    }
+
+    if (airCubes.has(coordsString)) {
+      // console.log('air already visited')
+      continue
+    }
+    // console.log({ coordsString })
+
+    if (rockCubes.has(coordsString)) {
+      // console.log('hit rock')
+      totalSurfaceArea++
+      continue
+    }
+
+    // console.log('add air')
+    airCubes.add(coordsString)
+    airToExplore.push(`${x - 1},${y},${z}`)
+    airToExplore.push(`${x + 1},${y},${z}`)
+    airToExplore.push(`${x},${y - 1},${z}`)
+    airToExplore.push(`${x},${y + 1},${z}`)
+    airToExplore.push(`${x},${y},${z - 1}`)
+    airToExplore.push(`${x},${y},${z + 1}`)
+  }
+}
+
 const main = () => {
   const input = getInput('input.txt')
   const addCube = (s: string) => {
-    cubes.set(s, new Cube(s))
+    rockCubes.set(s, new RockCube(s))
   }
+
   for (let line of input) {
     addCube(line)
   }
-  // console.log({ cubes })
-  let totalNeighbors = 0
-  for (let cube of cubes.values()) {
-    totalNeighbors += cube.neighborCount
+
+  if (input.length === 26) {
+    // example input
+    min = 0
+    max = 7
+    fillAir('0,0,0')
+  } else {
+    // printBounds()
+    // { minX: 0, maxX: 19, minY: 0, maxY: 19, minZ: 0, maxZ: 19 }
+    // full input
+    min = -1
+    max = 20
+    fillAir('-1,-1,-1')
   }
-  return cubes.size * 6 - totalNeighbors
+
+  return totalSurfaceArea
 }
 
 export { main }
