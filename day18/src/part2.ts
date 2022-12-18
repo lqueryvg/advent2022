5bd5a2bef25e394aa1659762330f22e068e0b347
John Buxton <lqueryvg@gmail.com>
2022-12-18 19:47:15 +0000

    day 18 part 1

--- day18/src/part2.ts
+++ day18/src/part2.ts
@@ -1,13 +1,57 @@
 import { readFileSync } from 'fs'
 
-const main = (filename: string) => {
-  const file = readFileSync(filename, 'utf-8')
+const getInput = (filename: string) => {
+  const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
+  return lines
+}
 
-  for (const line of lines) {
-    if (line === '') continue // skip blank lines
+const cubes = new Map<string, Cube>()
+class Cube {
+  x: number
+  y: number
+  z: number
+  // neighbors: Cube[]
+  neighborCount = 0
+
+  updateNeighborCountIfExists(x: number, y: number, z: number) {
+    const s = `${x},${y},${z}`
+    // console.log({ x, y, z })
+    if (cubes.has(s)) {
+      // console.log('yes')
+      cubes.get(s)!.neighborCount++
+      return 1
+    }
+    return 0
   }
-  return 'TBD'
+  constructor(thisString: string) {
+    const [x, y, z] = thisString.split(',').map(Number)
+    this.x = x
+    this.y = y
+    this.z = z
+    this.neighborCount += this.updateNeighborCountIfExists(x - 1, y, z)
+    this.neighborCount += this.updateNeighborCountIfExists(x + 1, y, z)
+    this.neighborCount += this.updateNeighborCountIfExists(x, y - 1, z)
+    this.neighborCount += this.updateNeighborCountIfExists(x, y + 1, z)
+    this.neighborCount += this.updateNeighborCountIfExists(x, y, z - 1)
+    this.neighborCount += this.updateNeighborCountIfExists(x, y, z + 1)
+  }
+}
+
+const main = () => {
+  const input = getInput('input.txt')
+  const addCube = (s: string) => {
+    cubes.set(s, new Cube(s))
+  }
+  for (let line of input) {
+    addCube(line)
+  }
+  // console.log({ cubes })
+  let totalNeighbors = 0
+  for (let cube of cubes.values()) {
+    totalNeighbors += cube.neighborCount
+  }
+  return cubes.size * 6 - totalNeighbors
 }
 
 export { main }
