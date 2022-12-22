df7c11e4d074d8f17c0394329626479695467f05
John Buxton <lqueryvg@gmail.com>
2022-12-22 13:12:42 +0000

    day 19 framework

--- day19/src/part1.ts
+++ day19/src/part1.ts
@@ -1,13 +1,35 @@
 import { readFileSync } from 'fs'
 
-const main = (filename: string) => {
+const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8')
   const lines = file.split(/\r\n|\n/)
 
-  for (const line of lines) {
-    if (line === '') continue // skip blank lines
-  }
-  return 'TBD'
+  //   const regexp =
+  //   /Valve (?<name>-?.*) has flow rate=(?<rate>-?.*); tunnels* leads* to valves* (?<exits>-?.*)/
+  // const nodes = lines.map((line) => {
+  //   const match = line.match(regexp)!.groups
+  //   return {
+  //     name: match!.name,
+  //     rate: Number(match!.rate),
+  //     isOpen: false,
+  //     exits: match!.exits.split(', '),
+  //   }
+  // })
+
+  // for (const line of lines) {
+  //   if (line === '') continue // skip blank lines
+  // }
+  return lines
+
+  // return {
+  //   jetPattern: line,
+  // }
+  // throw new Error('no input lines found')
+}
+
+const main = () => {
+  const input = getInput('input.txt')
+  return 'TBA'
 }
 
 export { main }
